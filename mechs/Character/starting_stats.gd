57bb8d5ca29e4d83d7905af4fbabc73fce198d51
MrBejvan <bejvaben@gmail.com>
2024-04-18 09:10:22 -0600

    Starting Stats and Sword Board Update
    Expanded Sword Board script and added starting stats file

--- mechs/Character/starting_stats.gd
+++ mechs/Character/starting_stats.gd
@@ -2,8 +2,8 @@ extends Resource
 
 class_name StartingStats
 
-export var max_health : int
-export var strength : int
-export var swift : int
-export var defense : int
+@export var max_health := 10
+@export var strength := 2
+@export var swift := 2
+@export var defense := 2
 
