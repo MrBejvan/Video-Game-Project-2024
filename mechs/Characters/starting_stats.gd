52519f4fef37981cfcf3fe13ddb8d7153400545f
MrBejvan <122756021+MrBejvan@users.noreply.github.com>
2024-04-19 16:54:46 -0600

    Update starting_stats.gd

--- mechs/Characters/starting_stats.gd
+++ mechs/Characters/starting_stats.gd
@@ -2,8 +2,10 @@ extends Resource
 
 class_name StartingStats
 
-@export var max_health := 10
-@export var strength := 2
-@export var swift := 2
-@export var defense := 2
+@export var max_health : int
+@export var strength : int
+@export var swift : int
+@export var defense : int
+
+@export var num_skills_available : int
 
