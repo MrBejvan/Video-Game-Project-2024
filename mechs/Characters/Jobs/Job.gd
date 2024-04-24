a4407405e817b466070496ffae6bd53a4df94333
MrBejvan <122756021+MrBejvan@users.noreply.github.com>
2024-04-24 15:19:13 -0600

    Goblin Setup for Success

--- mechs/Characters/Jobs/Job.gd
+++ mechs/Characters/Jobs/Job.gd
@@ -5,14 +5,15 @@ class_name Job
 @onready var stats = $Stats
 @onready var skills = $Skills
 
-@export var starting_stats : Resource
+@export var starting_stats : StartingStats
+
 @export var starting_skills : Array
 # @export var character_skill_scene : 
 
 #....
 
 func _ready():
-	stats.initialize(StartingStats)
+	stats.initialize(starting_stats)
 	if starting_skills != null and starting_skills.size() > 0:
 		#var new_skill = character_skill_scene.instance()
 		#new_skill.initialize(skill)
