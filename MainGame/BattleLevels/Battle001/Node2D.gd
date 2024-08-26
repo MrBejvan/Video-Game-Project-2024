c726610e10b300d47316709db6d2151054845fa7
MrBejvan <122756021+MrBejvan@users.noreply.github.com>
2024-08-25 18:39:44 -0600

    More progress/organization on target selection

--- MainGame/BattleLevels/Battle001/Node2D.gd
+++ MainGame/BattleLevels/Battle001/Node2D.gd
@@ -1,2 +1,4 @@
 extends Node2D
 
+func _process(delta):
+	self.position = $SelectionArrowSprite/SelctionArrowMarker2.position
--- MainGame/BattleLevels/Battle001/Target Selection.gd
+++ MainGame/BattleLevels/Battle001/Target Selection.gd
@@ -4,7 +4,7 @@ var target
 var arrowPos
 var bpsDict = {}
 var bpsKeysArr = []
-
+var selecting_target = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,13 +20,22 @@ func selectTarget():
 	arrowPos = bpsDict[currentKey]
 	
 	
-	emit_signal("attack", target)
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var current_target_index = 0
 	if selecting_target:
 		if Input.is_action_just_pressed("ui_right"):
-			current_target_index
+			current_target_index = current_target_index + 1 % bpsKeysArr.size()
+		elif Input.is_action_just_pressed("ui_left"):
+			current_target_index = (current_target_index - 1 + bpsKeysArr.size()) % bpsKeysArr.size()
+		elif Input.is_action_just_pressed("ui_accept"):
+			selecting_target = false
+			var selected_key = bpsKeysArr[current_target_index]
+			target = bpsDict[selected_key]
+			emit_signal("attack", target)
+			
 
 
 func _on_action_menu_button_attack():
